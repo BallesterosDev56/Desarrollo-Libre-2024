8c042949aa1113fbde6f61627b282962b1e42728
Daniick56 <ballesterosdeveloper56@gmail.com>
2024-09-24 18:34:44 -0500

    saving changes

--- frontend/src/components/bikeCard/BikeCard.jsx
+++ frontend/src/components/bikeCard/BikeCard.jsx
@@ -1,8 +1,21 @@
+import './bikeCard.css'
 
+export const BikeCard = ({imagen, regional})=> {
 
-export const BikeCard = ()=> {
-    return(
+    return (
         <>
+            <div className="card-container">
+                <img className='image-card' src={imagen} alt="image-card" />
+                <div className="flex-block">
+                    <p><strong>{regional}</strong><span className='soft-text'>({quantity})·{country}</span></p>
+                </div>
+                <div className="description">
+                    <p>{description}</p>
+                </div>
+                <div className="button--container">
+                    <button className='btn btn-success'>Alquilar</button>
+                </div>
+            </div>
         </>
     )
 }
