7a6986d980a60372b755600bcc5bf7467c626e56
ratprogramer <lsamuelmartinezruiz43@gmail.com>
2024-09-26 08:39:55 -0500

    Back v3.3 dia 3

--- frontend/src/logic/fetchEvents/FetchEvents.jsx
+++ frontend/src/logic/fetchEvents/FetchEvents.jsx
@@ -14,3 +14,22 @@ export const fetchEvents = async (region) => {
   }
   setLoading(false);
 };
+
+
+export const fetchCreateEvents = async (datos) => {
+    try {
+        let response = await fetch('http://localhost:3000/events', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify(datos)
+        })
+        let data = await response.json();
+        
+        return data;
+
+    } catch (error) {
+        console.log(error);
+    }
+};
